0c992d268d598851052bbbfb7489ee3149a66dd5
Felipe Madero <felipe.madero@gmail.com>
2024-12-31 12:45:56 -0300

    pass a logger to NewNetwork, so as to give control on where to redirect the logs

--- relayer/main/main.go
+++ relayer/main/main.go
@@ -138,6 +138,14 @@ func main() {
 	if logLevel <= logging.Debug {
 		networkLogLevel = logLevel
 	}
+	networkLogger := logging.NewLogger(
+		"p2p-network",
+		logging.NewWrappedCore(
+			networkLogLevel,
+			os.Stdout,
+			logging.JSON.ConsoleEncoder(),
+		),
+	)
 
 	// Initialize message creator passed down to relayers for creating app requests.
 	// We do not collect metrics for the message creator.
@@ -163,7 +171,7 @@ func main() {
 	}
 
 	network, err := peers.NewNetwork(
-		networkLogLevel,
+		networkLogger,
 		registerer,
 		nil,
 		manuallyTrackedPeers,
