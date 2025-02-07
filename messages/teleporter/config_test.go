e0a6e0b154e526784838d63dad657837c25dd139
Richard Pringle <richard.pringle@avalabs.org>
2025-02-07 13:42:30 -0500

    Don't go through json

--- messages/teleporter/config_test.go
+++ messages/teleporter/config_test.go
@@ -10,32 +10,47 @@ import (
 )
 
 func TestConfigValidate(t *testing.T) {
-	testCases := []struct {
-		name          string
-		rewardAddress string
-		isError       bool
-	}{
+	type test struct {
+		name     string
+		settings map[string]any
+		isError  bool
+	}
+
+	validAddress := "0x27aE10273D17Cd7e80de8580A51f476960626e5f"
+
+	testCases := []test{
 		{
-			name:          "valid",
-			rewardAddress: "0x27aE10273D17Cd7e80de8580A51f476960626e5f",
-			isError:       false,
+			name: "valid",
+			settings: map[string]any{
+				"reward-address": validAddress,
+			},
+			isError: false,
 		},
 		{
-			name:          "invalid",
-			rewardAddress: "0x27aE10273D17Cd7e80de8580A51f476960626e5",
-			isError:       true,
+			name: "invalid address",
+			settings: map[string]any{
+				"reward-address": validAddress[:len(validAddress)-1],
+			},
+			isError: true,
+		},
+		{
+			name: "invalid key",
+			settings: map[string]any{
+				"rewardAddress": validAddress,
+			},
+			isError: true,
 		},
 	}
 
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
-			c := &Config{
-				RewardAddress: test.rewardAddress,
-			}
-			err := c.Validate()
+			c, err := ConfigFromMap(test.settings)
+
 			if test.isError {
+				require.Nil(t, c)
 				require.Error(t, err)
 			} else {
+				require.NotNil(t, c)
 				require.NoError(t, err)
 			}
 		})
