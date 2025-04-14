5918f0f429fc222091186c84f680a634eb60717a
Michael Kaplan <michael@avalabs.org>
2025-04-14 13:05:04 -0400

    Configuration unit test fix

--- relayer/config/test_utils.go
+++ relayer/config/test_utils.go
@@ -67,6 +67,8 @@ var (
 				AccountPrivateKey: "0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027",
 			},
 		},
+		InitialConnectionTimeoutSeconds: 300,
+		MaxConcurrentMessages:           250,
 	}
 	TestValidSourceBlockchainConfig = SourceBlockchain{
 		RPCEndpoint: basecfg.APIConfig{
