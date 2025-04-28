bc4bda236fc18bec19c4aa7c6931b056f4e82034
Ian Suvak <ian.suvak@avalabs.org>
2025-04-28 12:22:22 -0400

    add unit tests, rename constants

--- signature-aggregator/aggregator/aggregator.go
+++ signature-aggregator/aggregator/aggregator.go
@@ -53,7 +53,7 @@ const (
 	minimumL1ValidatorBalance = 2048 * units.NanoAvax
 
 	// The amount of time to cache L1 validator balances in seconds
-	l1ValidatorBalanceCacheTTL = uint64(2)
+	l1ValidatorBalanceTTLSeconds = uint64(2)
 )
 
 var (
@@ -105,7 +105,7 @@ func NewSignatureAggregator(
 		messageCreator:           messageCreator,
 		pChainClient:             pChainClient,
 		pChainClientOptions:      pChainClientOptions,
-		currentL1ValidatorsCache: cache.NewTTLCache[ids.ID, []platformvm.APIL1Validator](l1ValidatorBalanceCacheTTL),
+		currentL1ValidatorsCache: cache.NewTTLCache[ids.ID, []platformvm.APIL1Validator](l1ValidatorBalanceTTLSeconds),
 	}
 	sa.currentRequestID.Store(rand.Uint32())
 	return &sa, nil
