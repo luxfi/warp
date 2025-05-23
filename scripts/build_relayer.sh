1434ac45a2de66bf8d30b485475c02451ba8ff6a
Ian Suvak <ian.suvak@avalabs.org>
2025-05-23 10:00:37 -0400

    replace panics and fix version flag

--- scripts/build_relayer.sh
+++ scripts/build_relayer.sh
@@ -54,6 +54,6 @@ fi
 
 cd $RELAYER_PATH
 # Build ICM Relayer, which is run as a standalone process
-last_git_tag=$(git describe --tags --abbrev=0 2>/dev/null) || last_git_tag="v0.0.0-dev"
+last_git_tag=$(git describe --tags --match "icm-relayer*" --abbrev=0 2>/dev/null) || last_git_tag="v0.0.0-dev"
 echo "Building ICM Relayer Version: $last_git_tag at $binary_path"
 go build -ldflags "-X 'main.version=$last_git_tag'" -o "$binary_path" "main/"*.go
