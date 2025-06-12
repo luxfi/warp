b1435dea4aafa1bdfe419abf9469d745aa9879b0
Geoff Stuart <geoffrey.stuart@avalabs.org>
2025-06-12 15:30:37 -0400

    Add comment to tools.go

--- relayer/tools.go
+++ relayer/tools.go
@@ -1,5 +1,9 @@
 // +build tools
 
+// This file exists to track tool dependencies for the project.
+// This file imports those dependencies, so that they are not removed by `go mod tidy`.
+// This allows dependabot to manage the dependency versions by having the dependencies in `go.mod`.
+
 package relayer
 
 import (
