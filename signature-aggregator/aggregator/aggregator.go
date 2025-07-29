36732c99c622c130a23446642f13b607a0f0b1a5
anviimishra <mishraanvii@gmail.com>
2025-07-29 11:35:31 -0400

    Ensure only odd requests are generated

--- signature-aggregator/aggregator/aggregator.go
+++ signature-aggregator/aggregator/aggregator.go
@@ -108,7 +108,7 @@ func NewSignatureAggregator(
 		pChainClientOptions:      pChainClientOptions,
 		currentL1ValidatorsCache: cache.NewTTLCache[ids.ID, []platformvmapi.APIL1Validator](l1ValidatorBalanceTTL),
 	}
-	sa.currentRequestID.Store(rand.Uint32())
+	sa.currentRequestID.Store(rand.Uint32() | 1)
 	return &sa, nil
 }
 
@@ -354,7 +354,7 @@ func (s *SignatureAggregator) CreateSignedMessage(
 	// Query the validators with retries. On each retry, query one node per unique BLS pubkey
 	operation := func() error {
 		// Construct the AppRequest
-		requestID := s.currentRequestID.Add(1)
+		requestID := s.currentRequestID.Add(2)
 		outMsg, err := s.messageCreator.AppRequest(
 			unsignedMessage.SourceChainID,
 			requestID,
