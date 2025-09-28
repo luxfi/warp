ef62b130daab121a0ab0d05b7ffae538d3f2b741
ylg <yang.linguan@avalabs.org>
2025-09-28 14:49:32 -0400

    support acp181

--- signature-aggregator/aggregator/aggregator.go
+++ signature-aggregator/aggregator/aggregator.go
@@ -118,13 +118,14 @@ func (s *SignatureAggregator) connectToQuorumValidators(
 	signingSubnet ids.ID,
 	quorumPercentage uint64,
 	skipCache bool,
+	destinationBlockchainID ids.ID,
 ) (*peers.CanonicalValidators, error) {
 	s.network.TrackSubnet(signingSubnet)
 
 	var vdrs *peers.CanonicalValidators
 	var err error
 	connectOp := func() error {
-		vdrs, err = s.network.GetCanonicalValidators(ctx, signingSubnet, skipCache)
+		vdrs, err = s.network.GetCanonicalValidators(ctx, signingSubnet, skipCache, destinationBlockchainID)
 		if err != nil {
 			msg := "Failed to fetch connected canonical validators"
 			log.Error(
@@ -550,6 +551,7 @@ func (s *SignatureAggregator) CreateSignedMessage(
 	requiredQuorumPercentage uint64,
 	quorumPercentageBuffer uint64,
 	skipCache bool,
+	destinationBlockchainID ids.ID,
 ) (*avalancheWarp.Message, error) {
 	// Validate quorum percentages
 	if err := validateQuorumPercentages(requiredQuorumPercentage, quorumPercentageBuffer); err != nil {
@@ -572,7 +574,7 @@ func (s *SignatureAggregator) CreateSignedMessage(
 		zap.Stringer("signingSubnet", signingSubnet),
 	)
 
-	vdrs, err := s.connectToQuorumValidators(ctx, log, signingSubnet, requiredQuorumPercentage, skipCache)
+	vdrs, err := s.connectToQuorumValidators(ctx, log, signingSubnet, requiredQuorumPercentage, skipCache, destinationBlockchainID)
 	if err != nil {
 		log.Error(
 			"Failed to fetch quorum of connected canonical validators",
