7376aa2bc1a58b2df6a9d67ee99f1b1f0d6f0223
Geoff Stuart <geoffrey.stuart@avalabs.org>
2025-10-14 12:07:50 -0400

    Fix test

--- peers/app_request_network_test.go
+++ peers/app_request_network_test.go
@@ -187,7 +187,7 @@ func TestTrackSubnets(t *testing.T) {
 	}
 	require.Zero(t, arNetwork.trackedSubnets.Len())
 	require.Zero(t, arNetwork.lruSubnets.Len())
-	mockValidatorClient.EXPECT().GetProposedValidators(gomock.Any(), gomock.Any()).Return(nil, nil).AnyTimes()
+	mockValidatorClient.EXPECT().GetCurrentCanonicalValidatorSet(gomock.Any(), gomock.Any()).Return(validators.WarpSet{}, nil).AnyTimes()
 	for range maxNumSubnets {
 		arNetwork.TrackSubnet(context.Background(), ids.GenerateTestID())
 	}
