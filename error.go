875c11af462a19e3de1db55e24d29a969919fc02
Zach Kelling <z@zeekay.io>
2025-12-11 23:04:08 -0800

    feat: add App* method aliases for backward compatibility

---
+++ error.go
@@ -0,0 +1,17 @@
+// Copyright (C) 2019-2025, Lux Industries Inc All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package warp
+
+import "fmt"
+
+// Error represents a warp error
+type Error struct {
+	Code    int32
+	Message string
+}
+
+// Error implements the error interface
+func (e *Error) Error() string {
+	return fmt.Sprintf("warp error %d: %s", e.Code, e.Message)
+}
